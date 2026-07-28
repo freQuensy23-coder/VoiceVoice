31098c3a6f3b7cccea8a9b468a8a350774b8c02a
Alexey Mametyev <64750224+freQuensy23-coder@users.noreply.github.com>
2026-07-28 18:50:33 +0200

    Fix Android test dependency alias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
